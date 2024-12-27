5edcdd88ee456d90cc6e0f81e65d91ae3e7facba
vineet2441 <jhavineet133@gmail.com>
2024-12-28 00:10:49 +0530

    Update Subarray.java

--- Subarray.java
+++ Subarray.java
@@ -1,24 +1,36 @@
 import java.util.Scanner;
-public class Subarray {
-    public static void maxSubArray(int[] nums){
-        int[] subnums=new int[nums.length];
-        int sum=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]>=0){
-                sum=nums[i]+sum;
+public class MaxMin {
+    public static int Mini(int[] nums,int N){
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<=N; i++){
+            if(nums[i]<min){
+                min=nums[i];
             }
         }
-        System.out.print("maximum sum of array "+sum);
+        return min;
     }
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int size=sc.nextInt();
-        int[] nums=new int[size];
-        for (int i=0; i<size; i++){
-            nums[i]=sc.nextInt();
+    public static int Max(int[] nums,int N){
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<=N; i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
         }
-        maxSubArray(nums);
+        return max;
 
     }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int size= sc.nextInt();
+        int [] nums= new int[size];
+        for(int i=0; i<=size;i++){
+            nums[i]=sc.nextInt();
+        }
+        int N=nums.length;
+        System.out.println("The maximum value is : "+ Max(nums, N));
+        System.out.println("The minimum value is : "+ Mini(nums, N));
+    }
+
     
 }
+
